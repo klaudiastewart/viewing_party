1bff9bc39fbf17c7bedf488e74378df861a1ac1b
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-20 16:13:03 -0600

    Refactor movie service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -8,9 +8,10 @@ class MovieService
   end
 
   def self.get_top_rated_movies
+    #set up a counter here to funnel the movies into an array 
     page_1 = conn.get('/3/movie/top_rated?&page=1')
     page_2 = conn.get('/3/movie/top_rated?&page=2')
-    require "pry"; binding.pry
+
     page_1_json = JSON.parse(page_1.body, symbolize_names: true)
     page_2_json = JSON.parse(page_2.body, symbolize_names: true)
     movie_list = page_1_json[:results] + page_2_json[:results]
@@ -18,22 +19,22 @@ class MovieService
 
 
   def self.list_movies_by_keyword(query)
-    page_1= conn.get("/3/search/movie?api_key=#{ENV['MOVIE_TOKEN']}&query=#{query}")
+    page_1= conn.get("/3/search/movie?query=#{query}")
     page_1_json = JSON.parse(page_1.body, symbolize_names: true)
     movie_list = page_1_json[:results]
   end
 
   def self.return_single_movie(id)
-    selected_movie = conn.get("/3/movie/#{id}?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US")
+    selected_movie = conn.get("/3/movie/#{id}")
     JSON.parse(selected_movie.body, symbolize_names: true)
   end
 
   def self.return_single_movie_review(id)
-    reviews = conn.get("/3/movie/#{id}/reviews?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US&page=1")
+    reviews = conn.get("/3/movie/#{id}/reviews")
     JSON.parse(reviews.body, symbolize_names: true)
   end
   def self.return_single_movie_cast(id)
-    cast = conn.get("/3/movie/#{id}/credits?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US&page=1")
+    cast = conn.get("/3/movie/#{id}/credits")
     JSON.parse(cast.body, symbolize_names: true)
   end
 end
