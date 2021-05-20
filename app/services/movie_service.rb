933f6fc2de8f7a6ed2c97d4f06792aaae14acb6a
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-20 15:56:48 -0600

    Refactor sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssservices

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -2,14 +2,14 @@ class MovieService
   def self.conn
     puts 'HELLO WORLD'
     Faraday.new(
-    url: "https://api.themoviedb.org/3/",
+    url: "https://api.themoviedb.org",
     params: { api_key: ENV['MOVIE_TOKEN'] }
     )
   end
 
   def self.get_top_rated_movies
-    page_1 = conn.get('movie/top_rated?&page=1')
-    page_2 = conn.get('movie/top_rated?&page=2')
+    page_1 = conn.get('/3/movie/top_rated?&page=1')
+    page_2 = conn.get('/3/movie/top_rated?&page=2')
 
     page_1_json = JSON.parse(page_1.body, symbolize_names: true)
     page_2_json = JSON.parse(page_2.body, symbolize_names: true)
@@ -18,22 +18,22 @@ class MovieService
 
 
   def self.list_movies_by_keyword(query)
-    page_1= conn.get("search/movie?api_key=#{ENV['MOVIE_TOKEN']}&query=#{query}")
+    page_1= conn.get("/3/search/movie?api_key=#{ENV['MOVIE_TOKEN']}&query=#{query}")
     page_1_json = JSON.parse(page_1.body, symbolize_names: true)
     movie_list = page_1_json[:results]
   end
 
   def self.return_single_movie(id)
-    selected_movie = conn.get("movie/#{id}?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US")
+    selected_movie = conn.get("/3/movie/#{id}?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US")
     JSON.parse(selected_movie.body, symbolize_names: true)
   end
 
   def self.return_single_movie_review(id)
-    reviews = conn.get("movie/#{id}/reviews?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US&page=1")
+    reviews = conn.get("/3/movie/#{id}/reviews?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US&page=1")
     JSON.parse(reviews.body, symbolize_names: true)
   end
   def self.return_single_movie_cast(id)
-    cast = conn.get("movie/#{id}/credits?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US&page=1")
+    cast = conn.get("/3/movie/#{id}/credits?api_key=#{ENV['MOVIE_TOKEN']}&language=en-US&page=1")
     JSON.parse(cast.body, symbolize_names: true)
   end
 end
