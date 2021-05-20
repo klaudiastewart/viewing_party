fb76c0f354fe1607ff3ca37df84ef4c216d9d73d
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-19 19:24:19 -0600

    Add private authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authorize
 
   def current_user
      @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -10,4 +11,12 @@ class ApplicationController < ActionController::Base
   end
 
    helper_method :current_user, :current_admin?
+
+   private
+
+   def authorize
+     unless User.find_by(id: session[:user_id])
+       redirect_to root_path, notice: 'You do not have access to that page, please log in.'
+    end
+  end
 end
