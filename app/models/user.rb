b507c161889d17c27a498f2db7dc688863f2c52a
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-19 19:28:05 -0600

    Add foreign key to relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates_confirmation_of :password
 
-  has_many :parties, dependent: :destroy
+  has_many :parties, dependent: :destroy, foreign_key: :host_id
   has_many :party_viewers, through: :parties
   has_many :user_friends, dependent: :destroy
   has_many :friends, through: :user_friends
@@ -14,11 +14,5 @@ class User < ApplicationRecord
   def invites
     id = self.id
     Party.joins(:party_viewers).where(party_viewers: { viewer_id: id })
-  end
-
-  def hosting
-    #find all the party ids(partyviewer) where the host id (parties) is = current_user.id
-    Party.joins(:party_viewers)
-    .where('viewer_id = "#{current_user[:id]}"')
   end
 end
